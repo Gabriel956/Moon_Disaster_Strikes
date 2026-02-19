51657be6495fdd3d63cef4a6724598900b883e7e
Gabriel956 <gobaez1@gmail.com>
2026-02-18 20:06:01 -0600

    Astro sprites

--- Assets/Scripts/player_movement.cs
+++ Assets/Scripts/player_movement.cs
@@ -21,6 +21,20 @@ public class player_movement : MonoBehaviour
         {
             body.linearVelocity = new Vector2(body.linearVelocity.x, jumpForce);
         }
+
+        ////speed = GameSettings.PlayerSpeed;
+        //float horizontalInput = Input.GetAxis("Horizontal");
+        //body.linearVelocity = new Vector2(horizontalInput * speed, body.linearVelocity.y);
+
+        ////Flip player when facing left/right.
+        //if (horizontalInput > 0.01f)
+        //    transform.localScale = new Vector3(-0.3f, 0.3f, 0.3f);
+
+        //else if (horizontalInput < -0.01f)
+        //    transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+
+        //if (Input.GetKey(KeyCode.Space))
+        //    body.linearVelocity = new Vector2(body.linearVelocity.x, speed);
     }
 
     void FixedUpdate()
