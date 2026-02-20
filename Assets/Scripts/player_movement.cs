75e16ed8f108131fcebf56a8f6ac64a48c294afc
Gabriel956 <gobaez1@gmail.com>
2026-02-19 20:42:25 -0600

    Got animations working

--- Assets/Scripts/player_movement.cs
+++ Assets/Scripts/player_movement.cs
@@ -2,39 +2,49 @@ using UnityEngine;
 
 public class player_movement : MonoBehaviour
 {
-    [SerializeField] private float speed = 5f;
-    [SerializeField] private float jumpForce = 7f;
-
+    //[SerializeField] private float speed = 5f;
+    //[SerializeField] private float jumpForce = 7f;
+    
     private Rigidbody2D body;
-
+    public float speed = 5f;
+    public float jumpForce = 7f;
+    Animator animator;
     void Start()
     {
+
+        animator = GetComponent<Animator>();
+        body = GetComponent<Rigidbody2D>();
+
         GameSettings.Load();
         speed = GameSettings.PlayerSpeed;
         jumpForce = GameSettings.PlayerJumpForce;
-        body = GetComponent<Rigidbody2D>();
+        
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    body.linearVelocity = new Vector2(body.linearVelocity.x, jumpForce);
+        //}
+
+        speed = GameSettings.PlayerSpeed;
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        body.linearVelocity = new Vector2(horizontalInput * speed, body.linearVelocity.y);
+
+        animator.SetFloat("speed", Mathf.Abs(horizontalInput));
+
+        //Flip player when facing left/right.
+        if (horizontalInput > 0.01f)
+            transform.localScale = new Vector3(-0.3f, 0.3f, 0.3f);
+
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+
+        if (Input.GetKey(KeyCode.Space))
             body.linearVelocity = new Vector2(body.linearVelocity.x, jumpForce);
-        }
-
-        ////speed = GameSettings.PlayerSpeed;
-        //float horizontalInput = Input.GetAxis("Horizontal");
-        //body.linearVelocity = new Vector2(horizontalInput * speed, body.linearVelocity.y);
-
-        ////Flip player when facing left/right.
-        //if (horizontalInput > 0.01f)
-        //    transform.localScale = new Vector3(-0.3f, 0.3f, 0.3f);
-
-        //else if (horizontalInput < -0.01f)
-        //    transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
-
-        //if (Input.GetKey(KeyCode.Space))
-        //    body.linearVelocity = new Vector2(body.linearVelocity.x, speed);
     }
 
     void FixedUpdate()
